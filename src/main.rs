6d8829f39ab2f825def80284deb9eb64183c2831
Matheus Santos <vorj.dux@gmail.com>
2022-04-11 20:56:06 +0100

    Draft implementation and testing some ideas to test the better options of the CLI parameters design

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,29 @@ fn cli() -> Command<'static> {
                 .about("txc:htmlencode|txc::htmlescape")
                 .arg(arg!(<INPUT> "Text to be converted").required(false)),
         )
+        .subcommand(
+            Command::new("uuid")
+                .about("txc:new_uuid"),
+        )
+        .subcommand(
+            Command::new("uuid1")
+                .about("txc:new_uuid1"),
+        )
+        .subcommand(
+            Command::new("uuid3")
+                .about("txc:new_uuid3")
+                .arg(arg!(-n --names <NAMES> "Names to be used in the UUID3 generation, separated by commas").default_value("foo").required(false))
+                .arg(arg!(-q --quantity <QUANTITY> "How many UUID3's to ge genrated").default_value("1").required(false))
+        )
+        .subcommand(
+            Command::new("uuid4")
+                .about("txc:new_uuid4")
+                .arg(arg!(-q --quantity <QUANTITY> "How many UUID4's to ge genrated").default_value("1").required(false)),
+        )
+        .subcommand(
+            Command::new("uuid5")
+                .about("txc:new_uuid5"),
+        )
 }
 
 fn process<I: IntoIterator<Item = String>>(strings: I, command: &str) {
@@ -41,7 +64,7 @@ fn process<I: IntoIterator<Item = String>>(strings: I, command: &str) {
     }
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let matches = cli().get_matches();
 
     use std::io::BufRead;
@@ -55,7 +78,46 @@ fn main() {
                     command,
                 ),
             }
-        }
+        },
+        Some((command @ ("uuid" | "uuid1" | "uuid2" | "uuid3" | "uuid4" | "uuid5"), sub_matches)) => {
+            
+            use uuid::Uuid;
+
+            let q = sub_matches.value_of("quantity").unwrap().parse::<usize>().expect("Invalid number");
+
+            match command {
+                "uuid" => println!("{}", Uuid::new_v4().to_hyphenated()),
+                "uuid1" => {
+                    use uuid::v1::{Timestamp, Context};
+                    let context = Context::new(42);
+                    let ts = Timestamp::from_unix(&context, 1497624119, 1234);
+                    let uuid = Uuid::new_v1(ts, &[1, 2, 3, 4, 5, 6]).expect("failed to generate UUID");
+                    println!("{}", uuid.to_hyphenated())
+                },
+                "uuid3" => {
+                    let names = sub_matches.value_of("names").unwrap().split(",").map(|s| s.to_string()).collect::<Vec<String>>();
+
+                    if names.len() != q {
+                        panic!("Invalid number of names");
+                    }
+                    
+                    (0..q).into_iter()
+                        .map(|i| Uuid::new_v3(&Uuid::NAMESPACE_DNS, names[i].as_bytes()).to_hyphenated())
+                        .for_each(|uuid| println!("{}", uuid))
+                },
+                "uuid4" => {
+                    let q = sub_matches.value_of("quantity").unwrap().parse::<usize>().expect("Invalid number");
+                    (0..q).into_iter()
+                        .map(|_| Uuid::new_v4().to_hyphenated())
+                        .for_each(|uuid| println!("{}", uuid))
+                },
+                "uuid5" => println!("{}", Uuid::new_v5(&Uuid::NAMESPACE_DNS, &[1, 2, 3, 4, 5, 6]).to_hyphenated()),
+                _ => {}
+            }
+        },
         _ => {}
-    }
+    };
+
+    Ok(())
+
 }
